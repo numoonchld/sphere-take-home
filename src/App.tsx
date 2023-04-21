f4b74d53e2c841348d4bd5d3a8d980d0f9dacae8
numoonchld <raghavendra.saralaya@gmail.com>
2023-04-22 01:34:35 +0530

    added wallet connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import AvailablePreSales from "./components/AvailablePreSales";
@@ -9,11 +8,32 @@ import PreSaleCreator from "./components/PreSaleCreator";
 declare const window: any;
 
 const App: FC = () => {
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [walletAddress, setWalletAddress] = useState<string>("");
+
+  const connectWallet = async () => {
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      setWalletAddress(response.publicKey.toString());
+      setIsConnected(true);
+    }
+  };
+
   return (
     <div className="App">
       <Sidebar />
       <section className="main-area">
-        <div className="user-login-area">| Discord | Wallet |</div>
+        <div className="user-login-area">
+          {!isConnected && (
+            <button onClick={connectWallet}>Connect Wallet</button>
+          )}
+          {isConnected && (
+            <i className="fa-solid fa-wallet">
+              &nbsp;&nbsp;&nbsp;&nbsp;{walletAddress}
+            </i>
+          )}
+        </div>
         <Routes>
           <Route path="/" element={<AvailablePreSales />} />
           <Route path="/creator" element={<PreSaleCreator />} />
