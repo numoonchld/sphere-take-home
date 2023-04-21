c69667124d72211f2ed70f591a0440745a0e3db0
numoonchld <raghavendra.saralaya@gmail.com>
2023-04-22 01:44:05 +0530

    submission draft

--- src/components/PreSaleCreator.tsx
+++ src/components/PreSaleCreator.tsx
@@ -1,7 +1,20 @@
 import React, { FC } from "react";
 
 const PreSaleCreator: FC = () => {
-  return <div>Pre-Sale Creator</div>;
+  return (
+    <div className="pre-sale-creator">
+      <h2>Pre-Sale Creator</h2>
+      <article>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
+          alt="Collection One"
+        />
+        <section>
+          <h3>Collection One</h3>
+        </section>
+      </article>
+    </div>
+  );
 };
 
 export default PreSaleCreator;
