bfd4a398bfe324ed21e5abbb8799e32ac9e898cf
numoonchld <raghavendra.saralaya@gmail.com>
2023-04-21 23:55:25 +0530

    added pretty icons to the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,16 +8,27 @@ const Sidebar: FC = () => {
         <i className="fa-solid fa-circle fa-5x"></i>
         <div className="nav-button-group">
           <Link to={"/"}>
-            <button className="nav-button --active">Available</button>
+            <button className="nav-button --active">
+              <i className="fa-solid fa-house"></i>
+              &nbsp;&nbsp;&nbsp;&nbsp;Available
+            </button>
           </Link>
           <Link to={"/"}>
-            <button className="nav-button">Claims</button>
+            <button className="nav-button">
+              <i className="fa-solid fa-box"></i>
+              &nbsp;&nbsp;&nbsp;&nbsp;Claims
+            </button>
           </Link>
           <Link to={"/creator"}>
-            <button className="nav-button">Creator</button>
+            <button className="nav-button">
+              <i className="fa-solid fa-pen"></i>&nbsp;&nbsp;&nbsp;&nbsp;Creator
+            </button>
           </Link>
           <Link to={"/"}>
-            <button className="nav-button">Settings</button>
+            <button className="nav-button">
+              <i className="fa-solid fa-sliders"></i>
+              &nbsp;&nbsp;&nbsp;&nbsp;Settings
+            </button>
           </Link>
         </div>
         <hr />
